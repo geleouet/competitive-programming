826ee607dab1e9a44446721dfb9b7073b3a4b9f6
mameil-meritis <49402780+mameil-meritis@users.noreply.github.com>
2019-04-10 10:02:18 +0200

    Update Exercice_4.cpp

--- battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
+++ battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <limits>
+#include <sstream>
+#include "exercise.hpp"
 
+using namespace std;
+#include<string>
+#include <vector>
+
+ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
+
+void ContestExerciseImpl::main() 
+{
+   int N; cin >> N;
+   
+}
