7cd1228f9a4ff9765d848e04d0c071b6522c5903
mameil-meritis <49402780+mameil-meritis@users.noreply.github.com>
2019-04-11 11:48:59 +0200

    Update Exercice_4.cpp

--- battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
+++ battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
@@ -9,40 +9,98 @@ using namespace std;
 
 ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
 
+// FONTION FACTORIELLE
+int fact(int n) {
+    if(n==0) {return 1;}
+    else {return n*fact(n-1);}
+}
+
+// FONCTION BINOMIALE
+int bin(int a, int b){
+    if(a>b) {return 0;}
+    else{return fact(b)/(fact(a)*fact(b-a));}
+}
+
+// FONCTION GENERATION DE COMBINAISONS D'ENTIERS DE TAILLE k
+vector<vector<int>> generer(int min, int max, int k) {
+    int nb = max - min + 1; // Le nombre d'entiers
+    if(k > nb) {return "NULL";} // On ne travaille pas si k est trop grand
+    else {
+        int size = bin(k, nb); // Le nombre de combinaisons possibles entre min et max
+        vector<vector<int>> comb[size];
+        
+        for(int i=min; i<= max-k+1; i++) {
+            int s = bin(k-1, max-i); // Le nombre de combinaisons de taille k commençant par i
+            
+            // Le nombre de combinaisons avant de s'intéresser à celles commençant par i
+            int n_comb = 0; 
+            for(int j=min; j<i; j++) {n_comb += bin(k-1, max-j);}
+            
+            // On va utiliser la récursivité
+            for(int t=0; t<s; t++) {
+                comb[n_comb+t][0] = i;
+                vector<int> extension[k-1] = generer(i+1, max, k-1)[t];
+                for(int m=0; m<k-1; m++) {comb[n_comb+t].insert(comb[n_comb+t].end(), extension[m]);}
+            }
+        }
+        return comb;
+    }
+}
+
+/// FONCTION GENERATION DE TOUTES LES COMBINAISONS
+vector<vector<int>> generer_all(int min, int max) {
+    int nb = max - min +1; // Le nombre d'entiers
+    
+    // Calcul du nombre total de combinaisons
+    int size = 0; 
+    for(int k=1; k<=nb; k++) {size += bin(k, nb);}
+    vector<vector<int>> all_comb[size];
+    
+    // Récupérer les combinaisons pour les insérer dans le vecteur all_comb
+    int n_comb = 0;
+    for(int k=1; k<=nb; k++) { // Pour chaque longueur de combinaison possible
+        int sk = bin(k, nb); // On récupère le nombre de combinaisons de cette taile
+        for(int i=0; i<bin(k, nb)) {all_comb[n_comb+i] = generer(min, max, k)[i];} // On récupère toutes les combinaisons
+        n_comb += bin(k, nb); // On a rajouté bin(k, nb) combinaisons dans le vecteur all_comb
+    }
+    return all_comb;
+}
+
 void ContestExerciseImpl::main() 
 {
     int N; cin >> N; // Le nombre de mots
-    int K = 10*N; // Le nombre de lettres
+    vector<vector<int>> Comb = generer_all(0, 9); // Les combinaisons croissantes des lettres (indexées par leur position)
+    vector<vector<string>> Var[N];
     
-    // On écrit les mots dans une seule chaîne de caractères
-    char Mots[K]; 
-    for(int k=0; k<K; k++) {cin >> Mots[k];}
-    
-    vector<vector<int>> Communs[N]; // Pour noter les positions des lettres, qui apparaissent dans tous les mots, dans chaque mot
-    
-    // On commence par regarder les lettres du premier mots, pour vérifier si elles apparaissent dans les autres mots
-    for(int l=0; l<10; l++) { // pour chaque lettre du premier mot 
-        vector<vector<int>> Prov[N]; // Pour noter provisoirement la position de la lettre dans les mots où elle apparaît
-        Prov[0].insert(Prov[0].end(), l); 
-    
-        // on vérifie que la lettre apparaît dans tous les mots
-        int n_mots = 1; // Pour retenir le nombre de mots dans lesquels la lettre étudiée apparaît
-        for(int m=1; m<N; m++) { // Pour chaque autre mot
-            int memory = n_mots; // on mémorise le nombre de mots dans lesquels la lettre apparaît jusqu'ici
-            int app = 0; // et on va compter le nombre de fois où elle apparaît dans ce mot
-            
-            for(int j=0; j<10; j++) { // Pour chaque lettre de cet autre mot
-                if(Mots[l] == Mots[10*m+j]) { // si c'est la lettre que l'on étudie
-                    Prov[m].insert(Prov[m].end(), j);
-                    app++; // on note qu'elle est apparue une fois de plus
-                }
-            }
-            
-            n_mots += (app!=0); // On note si la lettre est apparue au moins une fois dans cet autre mot
-            if(memory == n_mots) {break;} // Si la lettre n'apparaît pas dans cet autre mot, on n'a pas besoin de regarder dans les autres mots
-        }
-        if(n_mots == N) { // Si la lettre apparaît bien dans tous les mots
-            Communs[0].insert(Communs[0].end(), l); // On note la position de la lettre dans le mot
+    for(int n=0; n<N; n++) { // Pour chaque mot
+        char mot[10]; for(int k=0; k<10; k++) {cin >> mot[k];} // on crée uune chaîne de caractères comprenant les lettres du mot
+        int nb_comb = 0; // Le nombre de combinaisons issues de ce mot jusqu'à présent
+        
+        for(int k=1; k<10; k++) { // On peut écrire des mots de taille 1 à 10
+            int sk = bin(k,10); // Il y a sk mots de taille k
+            for(int i=0; i<sk; i++) {for(int j=0; j<k; j++) {Var[n][i] += mot[Comb[nb_comb+i][j]];}} // On les récupère tous à l'aide du vecteur Comb
+            nb_comb += sk;
         }
     }
+    
+    // RECHERCHE DU PLUS LONG MOT QUI APPARAIT DANS TOUS LES VECTEURS DE string
+    int m=1022 // On commence en regardant le mot le plus long dans le premier vecteur, puis on regarde des mots de plus en plus courts
+    int owned = 1; // Le nombre de vecteurs dans lequels le mot n°m du premier vecteur apparaît (il apparaît au moins une fois)
+    while(owned != N) {
+        string mot = Var[0][m]; // On enregistre le mot n°m du premier vecteur
+        for(int n=1; n<N; n++) { // Pour chaque vecteur autre que le premier vecteur
+            int memory = owned; // On retient le nombre de vecteurs dans lequel  
+            for(int i=1022; i<=0; i--) { // On va regarder les mots qu'il possède dans l'ordre inverse
+                string mot2 = Var[n][i]; // On enregistre son mot n°i
+                if(mot == mot2) {owned++; break;} // S'il est égal au mot qu'on étudie, on le note et on peut passer au vecteur suivant
+            }
+            if(memory == owned) { // Si le mot n'apparaît pas dans le vecteur n°n
+                m--; // On passe à un autre mot dans le premier 
+                owned = 1; // et owned reprend la valeur 1;
+            }
+        }
+    }
+    
+    cout << mot;
 }
+
