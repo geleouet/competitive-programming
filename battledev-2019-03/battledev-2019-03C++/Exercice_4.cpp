7de044e94febdc1ad003dcff34e95805bb7451f1
mameil-meritis <49402780+mameil-meritis@users.noreply.github.com>
2019-04-12 14:29:34 +0200

    Update Exercice_4.cpp

--- battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
+++ battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
@@ -42,7 +42,7 @@ vector<vector<int> > generer(int min, int max, int k)
     for(int i=0; i<size; i++) {comb[i] = vector<int>(k);}                       // On a donc des sous-vecteurs de tailles k
     
     if(k==1) {                                                                  // Si k vaut 1
-        for(int i=min; i<=max; i++) {                                           // chaque sous_vecteur
+        for(int i=min; i<=max; i++) {                                           // chaque sous-vecteur
             comb[i-min] = vector<int>(1);                                       // est de taille 1
             comb[i-min][0] = i;                                                 // et contient un entier entre min et max
         } 
