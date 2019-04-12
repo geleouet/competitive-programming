a4b8da0de44b52878dcf901749245efecb4ce715
mameil-meritis <49402780+mameil-meritis@users.noreply.github.com>
2019-04-12 12:31:43 +0200

    Update Exercice_2.cpp

--- battledev-2019-03/battledev-2019-03C++/Exercice_2.cpp
+++ battledev-2019-03/battledev-2019-03C++/Exercice_2.cpp
@@ -7,23 +7,22 @@ ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
 
 void ContestExerciseImpl::main() 
 {
-    int ARs = 0; int poids = 0; int n = 1; // Au début on a fait 0 aller-retour, le poids sur le monte-charge est nulle et on s'occupe du carton n°1
-    int N, p; // Le nombre total de cartons et le poids du carton "du moment" 
-    cin >> N; // On récupère le nombre total de cartons
+    int ARs = 0; int poids = 0; int n = 1;      // Au début on a fait 0 aller-retour, le poids sur le monte-charge est nulle et on s'occupe du carton n°1
+    int N, p;                                   // Le nombre total de cartons et le poids du carton "du moment" 
+    cin >> N;                                   // On récupère le nombre total de cartons
 
-    while(n <= N) { // Tant qu'il reste des cartons
-        cin >> p; // on récupère le poids du carton "du moment"
+    while(n <= N) {                             // Tant qu'il reste des cartons
+        cin >> p;                               // on récupère le poids du carton "du moment"
 
-        if(p <= 100-poids) { // Si on ne dépasse pas les 100kg avec le nouveau carton
-            poids += p; // on l'ajoute au monte-charge
+        if(p <= 100-poids) {                    // Si on ne dépasse pas les 100kg avec le nouveau carton
+            poids += p;                         // on l'ajoute au monte-charge
         }
-        else { // Sinon
-            ARs++; // on fait un aller-retour avec le monte-charge
-            poids = p; // et quand le monte-charge revient on met le carton dessus
+        else {                                  // Sinon
+            ARs++;                              // on fait un aller-retour avec le monte-charge
+            poids = p;                          // et quand le monte-charge revient on met le carton dessus
         }
-        n++; // Puis on s'occupe du carton suivant
+        n++;                                    // Puis on s'occupe du carton suivant
     }
-    ARs++; // Pour finir il faut tenir compte du dernier-aller retour, celui avec le dernier carton (n=N)
-
+    ARs++;                                      // Pour finir il faut tenir compte du dernier-aller retour, celui avec le dernier carton (n=N)
     cout << ARs;
 }
