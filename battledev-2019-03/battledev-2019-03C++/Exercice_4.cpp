899b4647dc97e685513f23f5d82f5bcc270d54da
mameil-meritis <49402780+mameil-meritis@users.noreply.github.com>
2019-04-12 11:43:28 +0200

    Update Exercice_4.cpp

--- battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
+++ battledev-2019-03/battledev-2019-03C++/Exercice_4.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
 
-
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // FONTION FACTORIELLE
 int fact(int n) {
@@ -23,7 +23,7 @@ int fact(int n) {
     else {return n*fact(n-1);}
 }
 
-
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // FONCTION BINOMIALE
 int bin(int a, int b){
@@ -31,105 +31,84 @@ int bin(int a, int b){
     else{return int(fact(b)/(fact(a)*fact(b-a)));}
 }
 
-
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // FONCTION GENERATION DE COMBINAISONS D'ENTIERS DE TAILLE k
 vector<vector<int> > generer(int min, int max, int k) 
 {
-    int nb = max - min + 1;                 // Le nombre d'entiers entre min et max
-    int size = bin(k, nb);                  // Le nombre de combinaisons de taille k possibles entre min et max
-    vector<vector<int> > comb(size);        // On les enregistrera dans ce vecteur
+    int nb = max - min + 1;                                                     // Le nombre d'entiers entre min et max
+    int size = bin(k, nb);                                                      // Le nombre de combinaisons de taille k possibles entre min et max
+    vector<vector<int> > comb(size);                                            // On les enregistrera dans ce vecteur
+    for(int i=0; i<size; i++) {comb[i] = vector<int>(k);}                       // On a donc des sous-vecteurs de tailles k
     
-    if(k==1) {                              // Si k vaut 1
-        for(int i=min; i<=max; i++) {       // chaque sous-vecteur (il y en a nb)
-            vector<int> actu(1);            // est de taille 1
-            actu[0] = i;                    // et contient un entier entre min et max
-            comb[i-min] = actu;
+    if(k==1) {                                                                  // Si k vaut 1
+        for(int i=min; i<=max; i++) {                                           // chaque sous_vecteur
+            comb[i-min] = vector<int>(1);                                       // est de taille 1
+            comb[i-min][0] = i;                                                    // et contient un entier entre min et max
         } 
     } 
-    
-    else {                                                              // Sinon...
-        for(int i=min; i<=max-k+1; i++)                                 // Une combinaison de taille k doit commmencer avant max-k+1
-        {
-            int n_comb = 0;                                             // Le nombre de combinaisons avant de s'intéresser à celles commençant par i
-            for(int j=min; j<i; j++) {n_comb += bin(k-1, max-j);}       // Pour chaque j avant i, il y a bin(k-1, max-j) combinaisons de taille j
-            
-            int s = bin(k-1, max-i);                                    // Le nombre de combinaisons de taille k commençant par i
-            
-            // On va utiliser la récursivité
-            vector<vector<int> > sub = generer(i+1, max, k-1);          // On récupère toutes les combinaisons de taille k-1 entre i+1 et max
-            for(int t=0; t<s; t++) {                                    // Pour chaque combinaison de taille k commençant par i
-                vector<int> actu(k);
-                actu[0] = i;                                            // Le premier élément est i
-                for(int m=0; m<k-1; m++) {                              // On regarde chaque élément de la combinaison de taille k-1
-                    int ext = sub[t][m];                                // On enregistre sa valeur
-                    actu[m+1] = ext;                                    // Et on l'ajoute à la combinaison de taille k commençant par i
-                }
-                comb[n_comb+t] = actu;
+    else { 
+        int n_comb = 0;                                                         // Le nombre de combinaisons de taille k étudiées jusqu'à présent
+        for(int i=min; i<=max-k+1; i++) {                                       // Une chaîne de taille k commence avant max-k+1
+            vector<vector<int> > sub = generer(i+1, max, k-1);                  // On récupère toutes les combinaisons de taille k-1 entre i+1 et max
+            int s = bin(k-1, max-i);                                            // Le nombre de combinaisons de taille k commençant par i
+            for(int t=0; t<s; t++) {                                            // Pour chaque combinaison de taille k commençant par i
+                comb[n_comb+t][0] = i;                                          // Le premier élément est i
+                for(int m=0; m<k-1; m++) {comb[n_comb+t][m+1] = sub[t][m];}     // Et on ajoute une combinaison de taille k-1 entre i+1 et max
             }
-        return comb;
+            n_comb += s;                                                        // On a étudié s combinaisons de plus
         }
     }
+    return comb;                                                                // On a toutes les combinaisons de taille k
 }
 
-
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // FONCTION GENERATION DE TOUTES LES COMBINAISONS
-vector<vector<int>> generer_all(int min, int max) 
+vector<vector<int> > generer_all(int min, int max) 
 {
-    int nb = max - min +1;                                                  // Le nombre d'entiers entre min et max
-    int size = 0; for(int k=1; k<=nb; k++) {size += bin(k, nb);}            // Le nombre total de combinaisons
-    vector<vector<int> > all_comb(size);                                    // Le vecteur dans lequel on va enregistrer toutes les combinaisons 
+    int nb = max - min +1;                                                      // Le nombre d'entiers entre min et max
+    int size = 0; for(int k=1; k<=nb; k++) {size += bin(k, nb);}                // Le nombre total de combinaisons
+    vector<vector<int> > all_comb(size);                                        // Le vecteur dans lequel on va enregistrer toutes les combinaisons 
 
     // Récupérer les combinaisons pour les insérer dans le vecteur all_comb
     int n_comb = 0;
-    for(int k=1; k<=nb; k++) {                                              // Pour chaque longueur de combinaison k possible
-        int sk = bin(k, nb);                                                // On récupère le nombre de combinaisons de taille k
-        vector<vector<int> > sub = generer(min, max, k);                     // On récupère les combinaisons dans un vecteur
-        for(int i=0; i<sk; i++) {                                           // Pour chaque combinaison de taille k
-            vector<int> actu(k);                                                    
-            for(int j=0; j<k; j++) {                                        // Pour chaque élément de la combinaison
-                int ext = sub[i][j];                                        // On enregistre sa valeur
-                actu[j] = ext;                                              // Et on l'ajoute à la combinaison dans le nouveau vecteur
-            }     
-            all_comb[n_comb+i] = actu;                                      // On récupère toutes les combinaisons
+    for(int k=1; k<=nb; k++) {                                                  // Pour chaque longueur de combinaison k possible
+        vector<vector<int> > sub = generer(min, max, k);                        // On récupère les combinaisons de taille k dans un vecteur sub
+        int sk = bin(k, nb);                                                    // Le nombre de combinaisons de taille k
+        for(int i=0; i<sk; i++) {                                               // Pour chaque combinaison de taille k
+            all_comb[n_comb+i] = vector<int>(k);                                // On a des sous-vecteurs de taille k               
+            for(int j=0; j<k; j++) {all_comb[n_comb+i][j] = sub[i][j];}         // On récupère toutes les combinaisons générées dans sub
         }     
-        n_comb += sk;                                                       // On a rajouté sk combinaisons dans le vecteur all_comb
+        n_comb += sk;                                                           // On a rajouté sk combinaisons dans le vecteur all_comb
     }
-    return all_comb;
+    return all_comb;                                                            // On a toutes les combinaisons possibles
 }
 
-
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // FONCTION MAIN
 void ContestExerciseImpl::main() 
 {
-    int N; cin >> N;                                        // Le nombre de mots
-    vector<vector<int> > Comb = generer_all(0, 9);          // Les combinaisons croissantes des lettres (indexées par leur position)
+    int N; cin >> N;                                                    // Le nombre de mots
+    vector<vector<int> > Comb = generer_all(0, 9);                      // Les combinaisons croissantes des lettres (indexées par leur position)
     
     // CREATION DE TOUS LES SOUS-MOTS POSSIBLES ISSUS DES N MOTS INITIAUX
-    vector<vector<string> > Var(N);                         // On va y stocker tous les mots possibles
-    for(int n=0; n<N; n++)                                  // Pour chaque mot initial
+    vector<vector<string> > Var(N);                                     // On va y stocker tous les mots possibles
+    for(int n=0; n<N; n++)                                              // Pour chaque mot initial
     { 
-        char mot_n[10];                                     // On écrit le mot sous forme de chaîne de caractères
-        for(int k=0; k<10; k++) {cin >> mot_n[k];}          // On met donc les lettres du mot dans la chaîne
+        Var[n] = vector<string>(1023)  ;                                // On peur créer 1023 sous-mots
+        char mot_n[10]; for(int k=0; k<10; k++) {cin >> mot_n[k];}      // On écrit le mot initial sous forme de chaîne de caractères
         
-        int nb_comb = 0;                                    // Le nombre de combinaisons issues de ce mot jusqu'à présent
-        
-        for(int k=1; k<=10; k++) {                          // On peut écrire des mots de taille 1 à 10
-            int sk = bin(k,10);                             // Il y a sk mots de taille k
-            for(int i=0; i<sk; i++) {                       // Pour créer un sous-mot
-            int pos = nb_comb+i;                            // La position du sous-mot dans le vecteur
-            vector<int> entiers = Comb[pos];                // La chaîne d'entiers correspondant à cette position
-            string provisoire;                              // Le string dans lequel on va ajouter toutes les lettres
-                for(int j=0; j<k; j++) {                    // Pour chaque lettre du sous-mot
-                    int pos_lettre = entiers[j];            // On récupère sa position dans la chaîne de caractère
-                    char lettre = mot_n[pos_lettre];        // On récupère sa "valeur" dans la chaîne de caractère
-                    provisoire += lettre;                   // On l'ajoute au sous-mot que l'on crée
+        int nb_comb = 0;                                                // Le nombre de combinaisons issues de ce mot jusqu'à présent
+        for(int k=1; k<=10; k++) {                                      // On peut écrire des mots de taille 1 à 10
+            int sk = bin(k,10);                                         // Il y a sk mots de taille k
+            for(int i=0; i<sk; i++) {                                   // Pour chaque combinaison de taille k
+                for(int j=0; j<k; j++) {                                // Pour chaque entier de cette combinaison
+                    Var[n][nb_comb+i] += mot_n[Comb[nb_comb+i][j]];      // On récupère la lettre correspondante et on l'ajoute au string 
                 }
-            Var[n][pos] = provisoire;                       // Et le sous-mot est crée
             } 
-            nb_comb += sk;                                  // On a ajouté sk combinaisons
+            nb_comb += sk;                                              // On a ajouté sk combinaisons
         }
     }
     
